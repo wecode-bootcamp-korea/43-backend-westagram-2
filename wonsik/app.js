1176f220865f410f853d3fe4b19460ad8e272db1
해동북 <hodozi@haedongbug-ui-MacBook-Pro.local>
2023-02-22 22:11:13 +0900

    [ADD]Express 초기 환경세팅

--- wonsik/app.js
+++ wonsik/app.js
@@ -32,6 +32,101 @@ app.get("/ping", (req, res) => {
   res.status(200).json({ message: "pong" });
 });
 
+//Create a book
+app.post("/books", async (req, res, next) => {
+  const { title, description, coverImage } = req.body;
+
+  await myDataSource.query(
+    `INSERT INTO books(
+      title,
+      description,
+      cover_image
+    )VALUES (?, ?, ?);
+    `,
+    [title, description, coverImage]
+  );
+
+  res.status(201).json({ message: "successfully created" });
+});
+
+//Create a author
+app.post("/authors", async (req, res) => {
+  const { first_name, last_name, age } = req.body;
+
+  await myDataSource.query(
+    `INSERT INTO authors(
+      first_name,
+      last_name,
+      age
+    )VALUES (?, ?, ?);
+    `,
+    [first_name, last_name, age]
+  );
+  res.status(201).json({ message: "successfully created" });
+});
+
+//Get all books
+app.get("/books", async (req, res) => {
+  // await myDataSource.manager.query(
+  //   `SELECT
+  //   b.id,
+  //   b.title,
+  //   b.description,
+  //   b.cover_image
+  //   FROM books b`,
+  //   (err, rows) => {
+  //     res.status(200).json(rows);
+  //   }
+  // );
+  const result = await myDataSource.manager.query(
+    `SELECT
+    b.id,
+    b.title,
+    b.description,
+    b.cover_image
+    FROM books b`
+  );
+
+  res.status(200).json(result);
+});
+
+//Get all books along with authors
+app.get("/books-with-authors", async (req, res) => {
+  await myDataSource.manager.query(
+    `SELECT
+    books.id,
+    books.title,
+    books.description,
+    books.cover_image,
+    authors.first_name,
+    authors.last_name,
+    authors.age
+    FROM books_authors ba
+    INNER JOIN authors ON ba.author_id = authors.id
+    INNER JOIN books ON ba.book_id = books.id`,
+    (err, rows) => {
+      res.status(200).json(rows);
+    }
+  );
+});
+
+// Update a single book by its primary key
+app.patch("/books", async (req, res) => {
+  const { title, description, cover_image, bookId } = req.body;
+
+  await myDataSource.query(
+    `UPDATE books
+    SET
+    title = ?,
+    description =?,
+    cover_image = ?
+    WHERE id = ?
+    `,
+    [title, description, cover_image, bookId]
+  );
+  res.status(201).json({ message: "successfully updated" });
+});
+
 const server = http.createServer(app);
 const PORT = process.env.PORT;
 
