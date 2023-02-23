f8821332713f195a7de4430c3ffb806b99501567
HyukJu <HyukJu@HyukJu.local>
2023-02-23 16:10:41 +0900

    [강혁주] Assingment 1: Express API - 초기 환경  세팅 - 리뷰 요청 반영 2

--- hyukju/app.js
+++ hyukju/app.js
@@ -1,4 +1,4 @@
-const dotenv = require("dotenv");
+const dotenv = require("dotenv"); //require("dotenv") .config()
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
@@ -7,10 +7,12 @@ const { DataSource } = require(`typeorm`);
 
 const app = express();
 
-app.use(logger("dev"));
 app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
 
 const myDataSource = new DataSource({
+  //my를 지우기
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -24,6 +26,22 @@ myDataSource.initialize().then(() => {
 });
 
 const PORT = process.env.PORT;
+
+app.post("/users", async (req, res, next) => {
+  const { name, email, profile_image, password } = req.body;
+  await myDataSource.query(
+    `INSERT INTO users(
+      name, 
+      email,
+      profile_image,
+      password
+      ) VALUES (?, ?, ?, ?);
+    `,
+    [name, email, profile_image, password]
+  );
+
+  res.status(201).json({ message: "successfully created" });
+});
 
 const start = async () => {
   app.listen(3000, () => {
