0e747c93166a5c01166b8f940e200694ca75ae5e
HyukJu <HyukJu@HyukJu.local>
2023-02-27 17:31:49 +0900

    [강혁주] Assignment 3: Express API - CRUD - 수정1

--- hyukju/app.js
+++ hyukju/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
 
-const DBDataSource = new DataSource({
+const dbDataSource = new DataSource({
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -19,15 +19,15 @@ const DBDataSource = new DataSource({
   database: process.env.DB_DATABASE,
 });
 
-DBDataSource.initialize().then(() => {
+dbDataSource.initialize().then(() => {
   console.log("Data Source has been initialized!");
 });
 
 const PORT = process.env.PORT;
 
-app.post("/users", async (req, res, next) => {
-  const { name, email, profile_image, password } = req.body;
-  await DBDataSource.query(
+app.post("/users", async (req, res) => {
+  const { name, email, profileImage, password } = req.body;
+  await dbDataSource.query(
     `INSERT INTO users(
       name, 
       email,
@@ -35,28 +35,28 @@ app.post("/users", async (req, res, next) => {
       password
       ) VALUES (?, ?, ?, ?);
     `,
-    [name, email, profile_image, password]
+    [name, email, profileImage, password]
   );
 
   res.status(201).json({ message: "successfully created" });
 });
 
-app.post("/posts", async (req, res, next) => {
-  const { title, content, user_id } = req.body;
-  await DBDataSource.query(
+app.post("/posts", async (req, res) => {
+  const { title, content, userId } = req.body;
+  await dbDataSource.query(
     `INSERT INTO posts(
       title, 
       content,
       user_id
       ) VALUES (?, ? ,?)
     `,
-    [title, content, user_id]
+    [title, content, userId]
   );
   res.status(201).json({ message: "successfully created" });
 });
 
-app.get("/users/posts/all", async (req, res) => {
-  await DBDataSource.query(
+app.get("/users/everyone/posts", async (req, res) => {
+  const users = await dbDataSource.query(
     `SELECT 
     users.id as userId,
     users.profile_image as userProfileImage,
@@ -66,16 +66,14 @@ app.get("/users/posts/all", async (req, res) => {
     FROM users
     INNER JOIN posts
     ON users.id = posts.user_id
-    `,
-    (err, rows) => {
-      res.status(200).json({ data: rows });
-    }
+    `
   );
+  res.status(200).json({ data: users });
 });
 
-app.get("/users/posts/oneperson/:userId", async (req, res) => {
+app.get("/users/:userId/posts", async (req, res) => {
   const { userId } = req.params;
-  const result = await DBDataSource.query(
+  const result = await dbDataSource.query(
     `SELECT users.id as userId, users.profile_image as userProfileImage, 
     JSON_ARRAYAGG(
       JSON_OBJECT(
@@ -97,10 +95,9 @@ app.get("/users/posts/oneperson/:userId", async (req, res) => {
 });
 
 app.patch("/posts/alter", async (req, res) => {
-  const { postId } = req.body; // /posts/:postId url부분에서 :postId부분만 내가 바꾸고 싶은 번호만 바꿔서 넣어주면 나중에 postId = "3" 이렇게 안해서 url상에 이미 정보가 있어서 결국 content 정보만 넣어주면 작동함
-  const { content } = req.body; //
+  const { postId } = req.body;
 
-  await DBDataSource.query(
+  await dbDataSource.query(
     `
     UPDATE posts
     SET content=? 
@@ -108,7 +105,7 @@ app.patch("/posts/alter", async (req, res) => {
     `,
     [content, postId]
   );
-  const result = await DBDataSource.query(
+  const result = await dbDataSource.query(
     `
       SELECT users.id as userId, users.name as userName, posts.id as postingId, posts.title as postingTitle, posts.content as postingContent
       FROM users
@@ -122,22 +119,22 @@ app.patch("/posts/alter", async (req, res) => {
   return res.status(200).json({ data: result });
 });
 
-app.patch("/posts/delete/:postId", async (req, res) => {
+app.delete("/posts/:postId/delete", async (req, res) => {
   const { postId } = req.params;
-
-  const result = await DBDataSource.query(
+  const result = await dbDataSource.query(
     `
       DELETE FROM posts
       WHERE posts.id = ?
     `,
     [postId]
   );
+
   return res.status(200).json({ message: "successfully created" });
 });
 
 app.post("/likes", async (req, res) => {
   const { userId, postId } = req.body;
-  const [isLiked] = await DBDataSource.query(
+  const [isLiked] = await dbDataSource.query(
     `
     SELECT id 
     FROM likes
@@ -148,7 +145,7 @@ app.post("/likes", async (req, res) => {
   );
 
   if (!isLiked) {
-    await DBDataSource.query(
+    await dbDataSource.query(
       `
       INSERT INTO likes (user_id, post_id)
       VALUES (?, ?)
@@ -157,7 +154,7 @@ app.post("/likes", async (req, res) => {
     );
     return res.status(200).json({ message: "LIKE_CREATED" });
   } else {
-    await DBDataSource.query(
+    await dbDataSource.query(
       `
     DELETE FROM likes
     WHERE user_id = ?
